aa74b6d27d544249c8d4c54a813787c9d96623b4
juicyyao <juicyyaoo@163.com>
2017-08-17 10:26:46 +0800

    提取英文单词

---
+++ crossin_exercise_10.7.py
@@ -0,0 +1,10 @@
+import re
+with open('from.txt','r') as f :
+    content=f.read()
+result=re.findall(r'[A-z]+',content)
+result=sorted(result)
+result1=''
+for lines in result:
+    result1+=lines+'\n'
+with open('to.txt','w') as fin:
+    fin.writelines(result)
